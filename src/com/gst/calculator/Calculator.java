e7196c3012770276ebc5581a2ba99118633b6da3
webobite <singhsubham510@gmail.com>
2020-06-15 13:35:40 +0530

    Fixed minor bugs

--- src/com/gst/calculator/Calculator.java
+++ src/com/gst/calculator/Calculator.java
@@ -1,23 +1,28 @@
 package com.gst.calculator;
 
-import java.util.ArrayList;
-import java.util.List;
+class Calculator {
 
-class Calculator { 
-	
-	
-	
-    public static void main (String[] args) { 
-      
-    	int unit = Integer.parseInt(args[0]);
-    	String commodity = args[1];
-    	int initialUnitPrice = Integer.parseInt(args[2]);
-    	GST gst = new GST();
-    	int gstPercent = gst.setGSTPercent(commodity);
-        
-        int gstAmount = gst.calculateGSTCharge(initialUnitPrice,10); 
-        System.out.println("Sum of two integer values :"+ gstAmount); 
-         
-        
-    } 
+	public static void main(String[] args) {
+
+		int unit = Integer.parseInt(args[0]);
+		String commodity = args[1];
+		int initialUnitPrice = Integer.parseInt(args[2]);
+		GST gst = new GST();
+
+		try {
+			int gstPercent = gst.setGSTPercent(commodity);
+			if (gstPercent != -1) {
+				int gstAmount = gst.calculateGSTCharge(initialUnitPrice, gstPercent);
+				System.out.println("GST Applicable Per Unit for " + commodity + " : " + gstAmount);
+				System.out.println("Final Price : " + ((unit * initialUnitPrice) + (unit * gstAmount)));
+			} else {
+				System.out.println(
+						"Commodity name : " + commodity + " entered by the User doesnot exist in software System.");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
--- src/com/gst/calculator/GSTCharge.java
+++ src/com/gst/calculator/GSTCharge.java
@@ -6,30 +6,44 @@ import java.util.List;
 class GST {
 
 	/**
-	 * Static constant for GST Percent of 4 categories of products
-	 * If you want to change the GST percent change here only. 
+	 * Static constant for GST Percent of 4 categories of products If you want
+	 * to change the GST percent change here only. It will be reflected in other
+	 * places too.
 	 */
 	private static final int foodGrainGSTPercent = 0;
 	private static final int furnitureGSTPercent = 5;
 	private static final int electronicsGSTPercent = 18;
 	private static final int cosmeticsGSTPercent = 28;
-	
+
 	/**
-	 * List of products 
+	 * List of products
 	 */
-	static List<String> products = new ArrayList<String>() {
+	static List<String> foodGrainProducts = new ArrayList<String>() {
 		{
 			add("Rice");
 			add("Wheat");
 			add("Dal");
+		}
+	};
+	static List<String> furnitureProducts = new ArrayList<String>() {
+		{
 
 			add("Table");
 			add("Sofa");
 			add("Chair");
+		}
+	};
 
+	static List<String> electronicProducts = new ArrayList<String>() {
+		{
 			add("Mobile");
 			add("TV");
 			add("Tablet");
+		}
+	};
+
+	static List<String> cosmeticsProducts = new ArrayList<String>() {
+		{
 
 			add("Cream");
 			add("Perfume");
@@ -37,45 +51,61 @@ class GST {
 		}
 	};
 
-
 	int gstCharge = 0;
-	
-	int getGSTPercent(int itemPos) {
-		if(itemPos >= 0 || itemPos <= 2) {
-			return foodGrainGSTPercent;
-		} else if (itemPos >= 3 || itemPos <= 5) {
-			return furnitureGSTPercent;
-		} else if (itemPos >= 6 || itemPos <= 8) {
-			return electronicsGSTPercent;
-		} else if (itemPos >= 9 || itemPos <= 11) {
-			return cosmeticsGSTPercent;
-		}
-		return -1;
-	}
 
 	/**
 	 * Method to set GST Percent according to commodity entered
+	 * 
 	 * @param commodity
 	 */
 	public int setGSTPercent(String commodity) {
-		
+
 		// if commodity entered doesn't match with existing record will return -1
 		int gstPercent = -1;
-		
-		System.out.println(products);
 
-		for (int i = 0; i < products.size(); i++) {
-			if (commodity.equals(products.get(i))) {
-				gstPercent = getGSTPercent(i);
-				return gstPercent;
+		/**
+		 * Search in foodGrainProducts for commodity
+		 */
+		for (String product : foodGrainProducts) {
+			if (product.contains(commodity)) {
+				return foodGrainGSTPercent;
 			}
 		}
+
+		/**
+		 * Search in furnitureProducts for commodity
+		 */
+		for (String product : furnitureProducts) {
+			if (product.contains(commodity)) {
+				return furnitureGSTPercent;
+			}
+		}
+
+		/**
+		 * Search in electronicProducts for commodity
+		 */
+		for (String product : electronicProducts) {
+			if (product.contains(commodity)) {
+				return electronicsGSTPercent;
+			}
+		}
+
+		/**
+		 * Search in cosmeticsProducts for commodity
+		 */
+		for (String product : cosmeticsProducts) {
+			if (product.contains(commodity)) {
+				return cosmeticsGSTPercent;
+			}
+		}
+
 		return gstPercent;
 
 	}
 
 	/**
 	 * Method to calculate GST charge
+	 * 
 	 * @param originalCost
 	 * @param gstPercent
 	 * @return gstCharge
